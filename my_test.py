1cba900c6f74230119ecbe4b37eb6f4cbcb5b64a
morpheusdaniel <202morpheus@gmail.com>
2016-10-16 17:29:26 +0900

    Add files via upload

---
+++ my_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# my_test.py
+
+import my_debugger
+
+debugger = my_debugger.debugger()
+
+pid = raw_input("Enter the PID of the process to attach to: ")
+
+debugger.attach(int(pid))
+
+debugger.run()
+
+debugger.detach()
+
+#debugger.load("C:\\WINDOWS\\system32\\calc.exe")
